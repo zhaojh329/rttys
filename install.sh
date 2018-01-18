ca5ae290a9ce05004d2593eb21ff3477054467b3
Jianhui Zhao <jianhuizhao329@gmail.com>
2018-01-18 23:08:28 +0800

    Optimize install script
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 update-rc.d rttys remove
 
-cp ../../../../bin/rttys /usr/sbin/
+which rttys > /dev/null && rm `which rttys`
+cp ../../../../bin/rttys /usr/local/sbin/
 cp rttys.init /etc/init.d/rttys
 update-rc.d rttys defaults
