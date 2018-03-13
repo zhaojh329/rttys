fd80dbe9883e49933c46457c230b87936e7b342e
Jianhui Zhao <jianhuizhao329@gmail.com>
2018-03-13 23:27:07 +0800

    Use ssl
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

--- install.sh
+++ install.sh
@@ -5,4 +5,11 @@ update-rc.d rttys remove
 which rttys > /dev/null && rm `which rttys`
 cp ../../../../bin/rttys /usr/local/sbin/
 cp rttys.init /etc/init.d/rttys
+
+rm -rf /etc/rttys
+
+mkdir /etc/rttys
+cp rtty.crt /etc/rttys
+cp rtty.key /etc/rttys
+
 update-rc.d rttys defaults
