7b163c1fbfff09c8d02e490e1d404b575a2f47d0
Jianhui Zhao <jianhuizhao329@gmail.com>
2018-03-14 22:51:32 +0800

    Fix build.sh
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-export GOARCH="amd64"
-
-go build
 cp -r root root_tmp
+mkdir -p root_tmp/usr/local/bin
+
+export GOARCH="amd64"
+go build
 mv rttys root_tmp/usr/local/bin
 tar zcvf rttys-x64.tar.gz -C root_tmp/ etc usr
 
 
 export GOARCH="386"
-
 go build
 mv rttys root_tmp/usr/local/bin
 tar zcvf rttys.tar.gz -C root_tmp/ etc usr
