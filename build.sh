55203a69625c0306d124b231464f39f02bcd654e
Jianhui Zhao <jianhuizhao329@gmail.com>
2018-06-14 17:03:00 +0800

    build.sh: Only build x64
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

--- build.sh
+++ build.sh
@@ -3,14 +3,7 @@
 cp -r root root_tmp
 mkdir -p root_tmp/usr/local/bin
 
-export GOARCH="amd64"
 go build
 mv rttys root_tmp/usr/local/bin
 tar zcvf rttys-x64.tar.gz -C root_tmp/ etc usr
-
-
-export GOARCH="386"
-go build
-mv rttys root_tmp/usr/local/bin
-tar zcvf rttys.tar.gz -C root_tmp/ etc usr
 rm root_tmp -r
