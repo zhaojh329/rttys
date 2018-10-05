dd572f2d33ba31ecd9718c1a311a9883070dde97
Jianhui Zhao <jianhuizhao329@gmail.com>
2018-10-05 15:13:46 +0800

    Update build.sh
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

--- build.sh
+++ build.sh
@@ -2,4 +2,30 @@
 
 targets=linux/386,linux/amd64,linux/arm,linux/arm64,linux/mips,linux/mips64,linux/mipsle,linux/mips64le,windows/*,darwin/*
 
-xgo --targets=$targets .
+xgo --targets=$targets -dest=bin .
+
+sudo chown -R `id -un` bin
+
+cd bin
+
+targets=$(ls)
+for t in $targets
+do
+	mv $t rttys
+	mkdir $t
+	mv rttys $t
+	cp ../conf/rttys.crt $t
+	cp ../conf/rttys.key $t
+
+	echo $t | grep "windows" > /dev/null
+	if [ $? -eq 0 ];
+	then
+		t=$(echo -n $t | sed 's/.exe//')
+		mv $t.exe $t
+		cp ../conf/rttys.ini $t
+	else
+		tar zcvf $t.tar.gz $t --remove-files
+	fi
+done
+
+cd -
