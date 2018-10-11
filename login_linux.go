ddf0c3c69ab7adf6d2b8194e9c40e09c56b85a3b
Jianhui Zhao <jianhuizhao329@gmail.com>
2018-10-11 23:07:07 +0800

    linux login: Reimplemented with pure Go
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

--- login_linux.go
+++ login_linux.go
@@ -1,58 +1,57 @@
 package main
 
 import (
-	"unsafe"
+	"errors"
+	"io/ioutil"
+	"strings"
 	"syscall"
+
+	"github.com/GehirnInc/crypt"
+	_ "github.com/GehirnInc/crypt/apr1_crypt"
+	_ "github.com/GehirnInc/crypt/md5_crypt"
+	_ "github.com/GehirnInc/crypt/sha256_crypt"
+	_ "github.com/GehirnInc/crypt/sha512_crypt"
 )
 
-/*
-#cgo CFLAGS: -D_GNU_SOURCE=1
-#cgo LDFLAGS: -lcrypt
-
-#include <stdlib.h>
-#include <shadow.h>
-#include <string.h>
-#include <unistd.h>
-#include <crypt.h>
-#include <stdbool.h>
-
-static bool login(const char *username, const char *password)
-{
-	struct spwd spw;
-	struct spwd *result;
-	struct crypt_data cdata;
-	char buf[1024], *sp;
-	int s;
-
-	if (!username || *username == 0)
-		return false;
-
-	s = getspnam_r(username, &spw, buf, sizeof(buf), &result);
-	if (s || !result)
-		return false;
-
-	cdata.initialized = 0;
-	sp = crypt_r(password, spw.sp_pwdp, &cdata);
-	if (!sp)
-		return false;
-
-	return !strcmp(sp, spw.sp_pwdp);
+type spwd struct {
+	sp_namp string
+	sp_pwdp string
 }
-*/
-import "C"
+
+func getspnam(name string) (*spwd, error) {
+	/* Disallow potentially-malicious user names */
+	if name == "" || name[0] == '.' || strings.Contains(name, "/") {
+		return nil, errors.New("Invalid")
+	}
+
+	data, err := ioutil.ReadFile("/etc/shadow")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, l := range strings.Split(string(data), "\n") {
+		if !strings.HasPrefix(l, name+":") {
+			continue
+		}
+
+		s := strings.Split(l, ":")
+
+		return &spwd{s[0], s[1]}, nil
+	}
+
+	return nil, errors.New("Not found")
+}
 
 func checkUser() bool {
-    return syscall.Getuid() == 0
+	return syscall.Getuid() == 0
 }
 
 func login(username, password string) bool {
-	c_username := C.CString(username)
-	c_password := C.CString(password)
+	sp, _ := getspnam(username)
+	if sp == nil {
+		return false
+	}
 
-	ok := C.login(c_username, c_password);
-
-	C.free(unsafe.Pointer(c_username))
-	C.free(unsafe.Pointer(c_password))
-
-	return bool(ok)
+	c := crypt.NewFromHash(sp.sp_pwdp)
+	return c.Verify(sp.sp_pwdp, []byte(password)) != nil
 }
