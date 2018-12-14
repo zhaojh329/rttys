1cd48ea77df6c5e6c6341252536c6baabc476ee3
Jianhui Zhao <jianhuizhao329@gmail.com>
2018-12-14 16:19:17 +0800

    Bump version 2.6.0
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

--- version.go
+++ version.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 const RTTYS_VERSION_MAJOR = 2
-const RTTYS_VERSION_MINOR = 5
-const RTTYS_VERSION_PATCH = 2
+const RTTYS_VERSION_MINOR = 6
+const RTTYS_VERSION_PATCH = 0
 
 func rttys_version() string {
     return fmt.Sprintf("%d.%d.%d", RTTYS_VERSION_MAJOR, RTTYS_VERSION_MINOR, RTTYS_VERSION_PATCH)
