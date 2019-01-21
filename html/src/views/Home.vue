73f627f0b7c7785c1aca4fa8a06a2ee39b286b72
Jianhui Zhao <jianhuizhao329@gmail.com>
2019-01-21 11:41:25 +0800

    Optimize js code by use Vue.set and Vue.delete
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

--- html/src/views/Home.vue
+++ html/src/views/Home.vue
@@ -250,17 +250,14 @@ export default {
             }
         },
         handleDelCmdEnv(key) {
-            delete this.cmdData.env[key];
-            this.cmdData.env = Object.assign({}, this.cmdData.env);
+            this.$delete(this.cmdData.env, key);
         },
         handleAddCmdEnv() {
             this.cmdData.currentEnv = this.cmdData.currentEnv.trim();
             if (this.cmdData.currentEnv != '') {
                 let e = this.cmdData.currentEnv.split('=');
-                if (e.length == 2) {
-                    this.cmdData.env[e[0]] = e[1];
-                    this.cmdData.env = Object.assign({}, this.cmdData.env);
-                }
+                if (e.length == 2)
+                    this.$set(this.cmdData.env, [e[0]], e[1]);
                 this.cmdData.currentEnv = '';
             }
         },
