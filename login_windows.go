8c839d42f274f428f185fc9949348d6f585ad392
Jianhui Zhao <jianhuizhao329@gmail.com>
2019-04-16 23:31:22 +0800

    StringToUTF16Ptr Deprecated. Use UTF16PtrFromString instead.
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

--- login_windows.go
+++ login_windows.go
@@ -15,11 +15,15 @@ func login(username, password string) bool {
 	LOGON32_PROVIDER_DEFAULT := 0
 	var token syscall.Handle
 
+	usernamePtr, _ := syscall.UTF16PtrFromString(username)
+	domainPtr, _ := syscall.UTF16PtrFromString(".")
+	passwordPtr, _ := syscall.UTF16PtrFromString(password)
+
 	proc := mod.NewProc("LogonUserW")
 	ret, _, err := proc.Call(
-		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(username))),
-		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr("."))),
-		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(password))),
+		uintptr(unsafe.Pointer(usernamePtr)),
+		uintptr(unsafe.Pointer(domainPtr)),
+		uintptr(unsafe.Pointer(passwordPtr)),
 		uintptr(LOGON32_LOGON_INTERACTIVE),
 		uintptr(LOGON32_PROVIDER_DEFAULT),
 		uintptr(unsafe.Pointer(&token)))
