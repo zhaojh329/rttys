11bf5e4f17619c9f931e1eb37bad05aa5830da58
Jianhui Zhao <zhaojh329@gmail.com>
2019-11-02 22:15:42 +0800

    Fix code style
    Signed-off-by: Jianhui Zhao <zhaojh329@gmail.com>

--- pwauth/auth_linux.go
+++ pwauth/auth_linux.go
@@ -7,28 +7,29 @@ import (
 )
 
 func auth(username, password string) error {
-    t, err := pam.StartFunc("rttys", username, func(s pam.Style, msg string) (string, error) {
-	switch s {
-	case pam.PromptEchoOff:
-	    return password, nil
-	case pam.PromptEchoOn:
-	    return password, nil
-	case pam.ErrorMsg:
-	    fmt.Print(msg)
-	    return "", nil
-	case pam.TextInfo:
-	    fmt.Println(msg)
-	    return "", nil
+	t, err := pam.StartFunc("rttys", username, func(s pam.Style, msg string) (string, error) {
+		switch s {
+		case pam.PromptEchoOff:
+			return password, nil
+		case pam.PromptEchoOn:
+			return password, nil
+		case pam.ErrorMsg:
+			fmt.Print(msg)
+			return "", nil
+		case pam.TextInfo:
+			fmt.Println(msg)
+			return "", nil
+		}
+		return "", errors.New("Unrecognized message style")
+	})
+	if err != nil {
+		return err
 	}
-	return "", errors.New("Unrecognized message style")
-    })
-    if err != nil {
-	return err
-    }
-    err = t.Authenticate(0)
-    if err != nil {
-	return err
-    }
-    //fmt.Println("Authentication succeeded!")
-    return nil
+
+	err = t.Authenticate(0)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
