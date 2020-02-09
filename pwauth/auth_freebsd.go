9875a70787934285c2a7d7450ee34b313528ac03
Jianhui Zhao <zhaojh329@gmail.com>
2020-02-09 22:30:50 +0800

    not implement auth_freebsd
    Signed-off-by: Jianhui Zhao <zhaojh329@gmail.com>

--- pwauth/auth_freebsd.go
+++ pwauth/auth_freebsd.go
@@ -2,56 +2,9 @@ package pwauth
 
 import (
 	"errors"
-	"io/ioutil"
-	"os"
-	"os/user"
-	"strings"
-
-	"github.com/GehirnInc/crypt"
-	_ "github.com/GehirnInc/crypt/apr1_crypt"
-	_ "github.com/GehirnInc/crypt/md5_crypt"
-	_ "github.com/GehirnInc/crypt/sha256_crypt"
-	_ "github.com/GehirnInc/crypt/sha512_crypt"
 )
 
-func getPassword(name string) (string, error) {
-	/* Disallow potentially-malicious user names */
-	if name == "" || name[0] == '.' || strings.Contains(name, "/") {
-		return "", errors.New("Invalid")
-	}
-
-	data, err := ioutil.ReadFile("/etc/master.passwd")
-	if err != nil {
-		return "", err
-	}
-
-	for _, l := range strings.Split(string(data), "\n") {
-		if !strings.HasPrefix(l, name+":") {
-			continue
-		}
-
-		s := strings.Split(l, ":")
-
-		return s[1], nil
-	}
-
-	return "", errors.New("Not found")
-}
-
+// Need to be implemented
 func auth(username, password string) error {
-	if os.Getuid() != 0 {
-		return errors.New("Cannot possibly work without effective root")
-	}
-
-	if _, err := user.Lookup(username); err != nil {
-		return err
-	}
-
-	pw, err := getPassword(username)
-	if err != nil {
-		return err
-	}
-
-	c := crypt.NewFromHash(pw)
-	return c.Verify(pw, []byte(password))
+	return errors.New("not implemented")
 }
