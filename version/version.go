12992acd019ca42a85d2bbb00a0567b7058fc423
Jianhui Zhao <zhaojh329@gmail.com>
2021-02-22 22:25:26 +0800

    fix: go-lint
    Signed-off-by: Jianhui Zhao <zhaojh329@gmail.com>

--- version/version.go
+++ version/version.go
@@ -7,14 +7,17 @@ var (
 	buildTime = ""
 )
 
+// Version return the version string
 func Version() string {
 	return version
 }
 
+// GitCommit return git commit on build
 func GitCommit() string {
 	return gitCommit
 }
 
+// BuildTime return build time
 func BuildTime() string {
 	return buildTime
 }
