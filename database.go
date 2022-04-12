03b976a9d15c0189487adc5992f6a0d9d581abc8
Jianhui Zhao <zhaojh329@gmail.com>
2022-04-12 17:24:17 +0800

    chore: fix code style
    Signed-off-by: Jianhui Zhao <zhaojh329@gmail.com>

--- database.go
+++ database.go
@@ -2,20 +2,20 @@
  * @Author: 周家建
  * @Mail: zhou_0611@163.com
  * @Date: 2021-07-27 19:02:39
- * @Description: 
+ * @Description:
  */
 
 package main
 
 import (
-	"strings"
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/mattn/go-sqlite3"
+	"strings"
 )
 
 func instanceDB(str string) (*sql.DB, error) {
-	sp:= strings.Split(str, "://")
+	sp := strings.Split(str, "://")
 	if len(sp) == 2 {
 		return sql.Open(sp[0], sp[1])
 	} else {
