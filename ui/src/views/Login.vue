7f1ff94f838c2cf446d1182fa7254931155feb37
Jianhui Zhao <zhaojh329@gmail.com>
2025-06-11 18:22:06 +0800

    chore: pretty Login page
    Signed-off-by: Jianhui Zhao <zhaojh329@gmail.com>

--- ui/src/views/Login.vue
+++ ui/src/views/Login.vue
@@ -3,9 +3,9 @@
     <template #header>
       {{ $t('Authorization Required') }}
     </template>
-    <el-form ref="form" :model="formValue" :rules="rules" label-width="80px" label-suffix=":" size="large">
-      <el-form-item :label="$t('Password')" prop="password">
-        <el-input type="password" v-model="formValue.password" prefix-icon="lock" :placeholder="$t('Enter password...')" @keyup.enter="handleSubmit" show-password/>
+    <el-form :model="formValue" size="large">
+      <el-form-item prop="password">
+        <el-input ref="passwordInput" type="password" v-model="formValue.password" prefix-icon="lock" :placeholder="$t('Enter password...')" show-password/>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" :loading="loading" @click="handleSubmit" class="login-button">{{ $t('Sign in') }}</el-button>
@@ -21,35 +21,30 @@ export default {
       loading: false,
       formValue: {
         password: ''
-      },
-      rules: {
-        password: {
-          trigger: 'blur',
-          message: () => this.$t('password is required')
-        }
       }
     }
   },
   methods: {
     handleSubmit() {
-      this.$refs.form.validate(valid => {
-        if (valid) {
-          const params = {
-            password: this.formValue.password
-          }
+      const params = {
+        password: this.formValue.password
+      }
 
-          this.axios.post('/signin', params).then(res => {
-            sessionStorage.setItem('rttys-sid', res.data.sid)
-            this.$router.push('/')
-          }).catch(() => {
-            this.$message.error(this.$t('Signin Fail! password wrong.'))
-          })
-        }
+      this.axios.post('/signin', params).then(res => {
+        sessionStorage.setItem('rttys-sid', res.data.sid)
+        this.$router.push('/')
+      }).catch(() => {
+        this.$message.error(this.$t('Signin Fail! password wrong.'))
       })
     }
   },
   created() {
     sessionStorage.removeItem('rttys-sid')
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.$refs.passwordInput.focus()
+    })
   }
 }
 </script>
@@ -60,7 +55,7 @@ export default {
 }
 
 .login {
-  width: 500px;
+  width: 400px;
   top: 40%;
   left: 50%;
   position: fixed;
@@ -69,11 +64,5 @@ export default {
 
 .login-button {
   width: 100%;
-}
-
-.copyright {
-  text-align: right;
-  font-size: 1.2em;
-  color: #888;
 }
 </style>
